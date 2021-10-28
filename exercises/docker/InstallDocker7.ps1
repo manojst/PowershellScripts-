90e5d8c094f4ef2b17f770ab358a3b37662b0461
Manojkumar Gnanasekaran(UST <172389@ust-global.com>
2021-10-28 16:55:48 +0530

    DotNet using Docker

---
+++ exercises/docker/InstallDocker7.ps1
@@ -0,0 +1,7 @@
+﻿$_url   = "https://download.docker.com/components/engine/windows-server/17.06/docker-17.06.2-ee-7.zip"
+$_dest  = "C:\Users\Administrator\Downloads\docker.zip"
+$_unzip = "C:\Program Files\Docker_unzip"
+Invoke-WebRequest -uri $_url -OutFile $_dest
+Expand-Archive -LiteralPath $_dest -DestinationPath $_unzip
+
+#"https://download.docker.com/components/engine/windows-server/17.06/docker-17.06.2-ee-7.zip" "C:\Users\Administrator\Downloads\docker.zip" "C:\Program Files\Docker_unzip"
---
+++ exercises/docker/UninstallDocker.ps1
@@ -0,0 +1,5 @@
+﻿Get-PackageProvider -Name *Docker*
+
+Uninstall-Package -Name docker -ProviderName DockerMsftProvider
+
+Uninstall-Module -Name DockerMsftProvider
